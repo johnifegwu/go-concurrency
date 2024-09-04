9913f2ab209993abf5d6d6d940d89bafeb4bf38f
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-09-04 12:06:18 +0100

    slices and maps

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johnifegwu/go-concurrency
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	crypto := map[string]float64{
+		"BTC":  64000.25,
+		"ETH":  3000,
+		"SHIB": 0.00055478,
+	}
+
+	// print the length of the ma
+	fmt.Printf("lenght: %v", len(crypto))
+
+	// print B
+	fmt.Printf("lenght: %v", len(crypto))
+	/*
+		// Slices
+		nameSlice := []string{"John", "Paul", "James"}
+
+		nameSlice = append(nameSlice, "Kalu")
+
+		// Say helo
+		for _, name := range nameSlice {
+			fmt.Printf("helo %v,\n", name)
+		}
+	*/
+
+	/*
+		count := 0
+
+		// Even ended numbers
+		for a := 1000; a <= 9999; a++ {
+
+			for b := 1000; b <= 9999; b++ {
+				n := a * b
+
+				// if a*b is even ended
+				s := fmt.Sprintf("%v", n)
+
+				if s[0] == s[len(s)-1] {
+
+					// increment count
+					count++
+				}
+
+			}
+		}
+
+		fmt.Println(count)
+	*/
+
+	/*
+		x, y := 3.4, 6.8
+
+		r := y * x
+
+		// Using fmt.Printf for formatted output
+		fmt.Printf("y=%v, type of %T\n", y, y)
+		fmt.Printf("x=%v, type of %T\n", x, x)
+		fmt.Printf("r=%v, type of %T\n", r, r)
+
+		for i := 1; i <= 20; i++ {
+			if i%3 == 0 && i%5 == 0 {
+				// if number is divisible by bith 3 and 5 print fizz buzz
+				fmt.Println("fizz buzz")
+			} else if i%3 == 0 {
+				// if number is divisible by 3 print fizz
+				fmt.Println("fizz")
+			} else if i%5 == 0 {
+				// if number is divisible by 5 print buzz
+				fmt.Println("buzz")
+			} else {
+				// print the number
+				fmt.Println(i)
+			}
+		}
+	*/
+}
